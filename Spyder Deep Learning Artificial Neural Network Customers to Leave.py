922cb17fbfa2dda4156049124447505c7d020cfd
metintorunoglu <45499397+metintorunoglu@users.noreply.github.com>
2019-05-04 01:46:50 +0300

    Add files via upload

---
+++ Spyder Deep Learning Artificial Neural Network Customers to Leave.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv('customers_info.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le_X1=LabelEncoder()
+X[:, 1]=le_X1.fit_transform(X[:, 1])
+le_X2=LabelEncoder()
+X[:, 2]=le_X2.fit_transform(X[:, 2])
+enc=OneHotEncoder(categorical_features=[1])
+X=enc.fit_transform(X).toarray()
+X=X[:, 1:]
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+classifier = Sequential()
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
